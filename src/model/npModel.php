c06f6ba7434d472c00bffc1e52317bf8e30ca419
Sonterix <sonterixo@gmail.com>
2018-01-06 14:45:54 +0200

    added "getDepartments"

--- src/model/npModel.php
+++ src/model/npModel.php
@@ -2,7 +2,7 @@
 
 namespace app\model;
 
-use \PDO;
+use \PDO as PDO;
 use app\model\DbModel as DbModel;
 
 class NpModel
@@ -50,7 +50,6 @@ class NpModel
 
         // Get respons data
         $data = json_decode($data, true);
-        $city = [];
 
         // Work with db
         $sqlCheckTable = $this->dbh->query("SHOW TABLES LIKE 'np_cities'");
@@ -60,7 +59,7 @@ class NpModel
         } else {
             $this->dbh->query("CREATE TABLE `np_cities` (
                 `id` INT(6) NOT NULL AUTO_INCREMENT,
-                `description` VARCHAR(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                `description_ua` VARCHAR(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                 `description_ru` VARCHAR(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                 `ref` VARCHAR(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                 `city_id` INT(6) NOT NULL,
@@ -71,9 +70,9 @@ class NpModel
 
         // Check data from respons
         foreach ($data['data'] as $key => $value) {
-            $sqlInsert = $this->dbh->prepare("INSERT INTO np_cities (description, description_ru, ref, city_id) VALUES(:description, :description_ru, :ref, :city_id)");
+            $sqlInsert = $this->dbh->prepare("INSERT INTO np_cities (description_ua, description_ru, ref, city_id) VALUES(:description_ua, :description_ru, :ref, :city_id)");
             $sqlInsert->execute([
-                ':description' => $value['Description'],
+                ':description_ua' => $value['Description'],
                 ':description_ru' => $value['DescriptionRu'],
                 ':ref' => $value['Ref'],
                 ':city_id' => $value['CityID']
@@ -84,7 +83,58 @@ class NpModel
     }
 
     public function getDepartments(){
-        echo 'getDepartments';
+        // cURL
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://api.novaposhta.ua/v2.0/json/AddressGeneral/getWarehouses');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-type: application/json'
+        ]);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode( [
+            "modelName" => "AddressGeneral",
+            "calledMethod" => "getWarehouses",
+            "methodProperties" => [
+                "Language" => "ru"
+            ],
+            "apiKey" => $this->conf['apiKey']
+        ]));
+
+        $data = curl_exec($ch); 
+        curl_close($ch);
+
+        // get respons data
+        $data = json_decode($data, true);
+
+        // Work with db
+        $sqlCheckTable = $this->dbh->query("SHOW TABLES LIKE 'np_departments'");
+
+        if($sqlCheckTable->rowCount()){
+            $this->dbh->query("TRUNCATE TABLE np_departments"); 
+        } else {
+            $this->dbh->query("CREATE TABLE `np_departments` (
+                `id` INT(6) NOT NULL AUTO_INCREMENT,
+                `description_ua` VARCHAR(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                `description_ru` VARCHAR(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                `ref` VARCHAR(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                `city_ref` VARCHAR(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                `date` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY(`id`)
+            ) ENGINE = InnoDB");
+        }
+
+        // check data from respons
+        foreach($data['data'] as $value){
+            $sqlInsert = $this->dbh->prepare("INSERT INTO np_departments (description_ua, description_ru, city_ref, ref ) VALUES(:description_ua, :description_ru, :ref, :city_ref)");
+            $sqlInsert->execute([
+                ':description_ua' => $value['Description'],
+                ':description_ru' => $value['DescriptionRu'],
+                ':ref' => $value['Ref'],
+                ':city_ref' => $value['CityRef']
+            ]);
+        }
+
         return true;
     }
 
